dce8bbc72b14930d6147db214a2a71222915af77
JINGYU PARK <airroket@outlook.com>
2020-09-17 22:38:15 +0900

    update text3

--- app/src/main/java/com/example/android_project_capstone/MainActivity.java
+++ app/src/main/java/com/example/android_project_capstone/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //구본성
+        //박진규
         setContentView(R.layout.activity_main);
         a = findViewById(R.id.testText);
     }
