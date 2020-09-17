9123cdc9db5b9331e5787a551733f521b6b03459
terry-koo <xpflxhfl94@naver.com>
2020-09-17 22:30:13 +0900

    구본성 주석 테스트

--- app/src/main/java/com/example/android_project_capstone/MainActivity.java
+++ app/src/main/java/com/example/android_project_capstone/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        //dfssdfs
+        //구본성
         setContentView(R.layout.activity_main);
         a = findViewById(R.id.testText);
     }
